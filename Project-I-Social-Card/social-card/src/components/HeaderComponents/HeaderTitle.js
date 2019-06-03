7ab166d16541aab8f72d4e62177bddf440e266e1
Kevin-S <kevsilvo@gmail.com>
2019-06-03 11:44:20 -0400

    social-style

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-const HeaderTitle = props => <h1>Lambda School</h1>;
+const HeaderTitle = props => {
+
+return(
+        <h1>Lambda School   <span>@LambdaSchool 26 Jan</span></h1>
+)
+
+};
 
 export default HeaderTitle;
