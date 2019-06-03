188f67bde63b8e4b62c2e79dc9aeb36e4c677563
Kevin-S <kevsilvo@gmail.com>
2019-06-03 11:29:42 -0400

    social-header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import  ImageThumbnail from'./ImageThumbnail.js';
+import HeaderContent from './HeaderContent.js';
+
+
+
+const HeaderContainer = props => {
+    return (
+        <section class="header">
+            <ImageThumbnail/>
+            <HeaderContent/>
+        </section>
+
+    )
+
+};
+
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from'./HeaderTitle';
+
+const HeaderContent = props => <div><HeaderTitle/><HiContent/></div>;
+const HiContent = props => {
+return (
+            <p>
+                Let's learn react by building simple interfaces
+                with components. Don't try to overthink it. Just 
+                keep it simple and have fun. Once you feel comfortable using
+                components you are well on your way to mastering React!
+            </p>
+        )
+};
+
+export default HeaderContent;
