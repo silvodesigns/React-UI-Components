2c98e1985d804e283f5516336c3ab45dafcfee60
Kevin-S <kevsilvo@gmail.com>
2019-06-03 12:27:38 -0400

    social-done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = props => {
+
+        return(
+             <section class=".card">
+                <CardBanner/>
+                <CardContent/>
+             </section>
+        )
+
+};
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+
+    return (
+        <div class="card-content">
+            <h3>Getting Started with React</h3>
+            <p>
+                React makes it painless to create interactive UI's. Design
+                Simple views for each state in your application.
+
+
+            </p>
+            <span>Reactjs.org</span>
+        </div>
+
+    )
+};
+
+export default CardContent;
