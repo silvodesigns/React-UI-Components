20fbcb2e9f8be8122671c0074a931c686bb5ecb5
Kevin-S <kevsilvo@gmail.com>
2019-06-03 12:30:44 -0400

    social-link

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,10 +6,12 @@ import CardContent from './CardContent';
 const CardContainer = props => {
 
         return(
+            <a href="https://www.reactjs.org">
              <section class=".card">
                 <CardBanner/>
                 <CardContent/>
              </section>
+             </a>
         )
 
 };
